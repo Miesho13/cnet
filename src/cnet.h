848234db8f4929731f15fea390c3e498f552654d
miesho <marcin.ryzewskii@gmail.com>
2024-02-11 14:13:54 +0100

    Add Apache 2.0 License and author information to cnet.h and cnet.c

--- src/cnet.h
+++ src/cnet.h
@@ -1,3 +1,27 @@
+/*
+ *  File: cnet.h
+ *  Author: marcin.ryzewskii@gmail.com 
+ * 
+ *  Copyright 2024 Marcin Ryzewski 
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ *  
+ *  Brief: This file contains the implementation of the CNET library.
+ *         It includes functions for establishing connections, sending and 
+ *         receiving data, and handling errors.
+ */
+
 #ifndef __CNET__
 #define __CNET__
 
@@ -33,7 +57,7 @@ typedef struct {
 /*
  * 
  */
-cnet_conn_t* cnet_client_open(char *uri, uint16_t port,
+cnet_conn_t* cnet_client_open(const char *uri, uint16_t port,
                               transport_layer_t tran);
 /*
  * 
