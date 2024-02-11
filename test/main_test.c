154cfe4cfad8374460e3de2791e1181a9d5d540a
marcin <marcin.ryzewskii@gmail.com>
2024-02-11 13:32:37 +0100

    add main_test.c, testing instance

---
+++ test/main_test.c
@@ -0,0 +1,20 @@
+#include "../src/cnet.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+
+
+int main(void) {
+    printf("test run...\n");
+
+//    cnet_peer_ctx_t *client = cnet_connect("localhost", 4242, TCP);
+//
+//    int byte_send = cnet_send(client, http_req, sizeof(http_req));
+
+//    uint8_t *http_respone = (uint8_t*)malloc(255);
+//    int byte_rcev = cnet_rcv(client, &http_respone);
+
+//    printf("%s", (char*)http_respone);
+//    cnet_close(client);
+}
