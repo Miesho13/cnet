039e46b488188e46a377ca568f7bd33c3ce2387f
Marcin <marcin.ryzewskii@gmail.com>
2024-12-29 23:07:16 +0100

    Add test demo for server site.

--- test/main_test.c
+++ test/main_test.c
@@ -7,14 +7,10 @@
 
 int main(void) {
     printf("test run...\n");
+    cnet_client *send = cnet_client_open_udp("127.0.0.1:6969");
+    char msg[] = "Troche XD\n";
+    cnet_client_send(send, msg, strlen(msg));
+    close(send->sockfd);
 
-//    cnet_peer_ctx_t *client = cnet_connect("localhost", 4242, TCP);
-//
-//    int byte_send = cnet_send(client, http_req, sizeof(http_req));
-
-//    uint8_t *http_respone = (uint8_t*)malloc(255);
-//    int byte_rcev = cnet_rcv(client, &http_respone);
-
-//    printf("%s", (char*)http_respone);
-//    cnet_close(client);
+    return 0;
 }
